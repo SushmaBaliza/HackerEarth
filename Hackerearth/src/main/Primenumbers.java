dc2e96ca6c550cd9eaa96a3d92d868910a906ca4
Sushma Baliza <sushma.baliza23@gmail.com>
2018-08-05 12:38:17 +0530

    Prime Numbers

---
+++ Hackerearth/src/main/Primenumbers.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.util.Scanner;
+
+public class Primenumbers {
+    public static void main (String[] args)
+    {
+        int i =0;
+        int num =0;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+
+        for (i = 1; i <= n; i++)
+        {
+            int counter=0;
+            for(num =i; num>=1; num--)
+            {
+                if(i%num==0)
+                {
+                    counter = counter + 1;
+                }
+            }
+            if (counter ==2)
+            {
+
+                System.out.print(i+" ");
+            }
+        }
+
+    }
+}
